2fa59f31e84eebdb1f70169c6cd839d90faaee62
xand <alepbravo1702@gmail.com>
2025-05-26 22:28:37 -0300

    codigo basico

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#include <Joystick.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Create the Joystick object
+Joystick_ Joystick;
+
+// Define pins
+const int X_AXIS_PIN = A0;
+const int Y_AXIS_PIN = A1;
+const int BUTTON_PIN = 2;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Initialize the joystick
+  Joystick.begin();
+  
+  // Set up the button pin as input with pull-up resistor
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Read button state (inverted because of pull-up)
+  bool buttonState = !digitalRead(BUTTON_PIN);
+  
+  // Update joystick values
+  Joystick.setXAxis(analogRead(X_AXIS_PIN));
+  Joystick.setYAxis(analogRead(Y_AXIS_PIN));
+  Joystick.setButton(0, buttonState);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Small delay to prevent too rapid updates
+  delay(10);
 }
